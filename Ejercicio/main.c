71464c1c981744ab76102fa648754c7295ddee70
alumno <alumno@lab6pc0>
2018-08-27 11:20:39 -0300

    declaracion de variables

---
+++ Ejercicio/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+un ejercicio que nos saque el numero del medio
+
+*/
+int main()
+{
+
+    int numeroUno;
+    int numeroDos;
+    int numeroTres;
+
+    printf("Hello world!\n");
+    return 0;
+}
